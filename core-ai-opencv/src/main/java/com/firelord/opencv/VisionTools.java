11f19aa2bf28fee9ae4019ebfc18e01dcc8ef50a
JHercules <JHercules_qz@qq.com>
2018-12-05 16:47:00 +0800

    update project

---
+++ core-ai-opencv/src/main/java/com/firelord/opencv/VisionTools.java
@@ -0,0 +1,17 @@
+package com.firelord.opencv;
+
+import com.firelord.opencv.image_basic_feature.ImgBasicFeature;
+import com.firelord.opencv.video.VisionVideo4PC;
+
+/**
+ * VisionTools
+ */
+public class VisionTools {
+    //#region Fields
+
+    public static ImgBasicFeature imgBasicFeature = new ImgBasicFeature();
+
+    public static VisionVideo4PC video4PC = new VisionVideo4PC();
+
+    //#endregion
+}
